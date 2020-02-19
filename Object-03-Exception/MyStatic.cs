4c4024685745ae0b10bc341d87aa2c3461d169d6
phoenix fate <sm516116978@outlook.com>
2020-02-19 13:49:42 +0800

    异常处理；常量；静态成员；静态方法；静态类

---
+++ Object-03-Exception/MyStatic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * 静态成员
+ * 静态方法
+ * 静态类
+ */
+namespace Object_03_Exception
+{
+    public class MyStatic
+    {
+        public static int totalCount;
+
+        public int age;
+
+        public static void Main(string[] args)
+        {
+            //在static方法中可以调用其他static成员，但不能调用非static成员
+            //age = 30;
+            totalCount = 30;
+            Console.WriteLine("totalCount: {0}", totalCount);
+            //静态类不能new
+            //ConsoleHelper consoleHelper = new ConsoleHelper();
+            Console.WriteLine("请输入一个整数");
+            int readInt = ConsoleHelper.ReadInt();
+            Console.WriteLine("刚才输入的整数是: {0}", readInt);
+        }
+    }
+
+    //静态类
+    static class ConsoleHelper
+    {
+        public static int ReadInt()
+        {
+            String str = Console.ReadLine();
+            return Convert.ToInt32(str);
+        }
+
+        //静态类中只能写静态方法
+        //public void SayHello()
+        //{
+
+        //}
+    }
+
+
+}
---
+++ Object-03-Exception/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+/*
+ * 异常
+ * 
+ */
+namespace Object_03_Exception
+{
+    class Program
+    {
+        public static double CLASS_PI = 3.14;
+
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const double PI = 3.1415;
+            double r = 5;
+            double area = PI * r * r;
+            // {0:f3} 限制小数点位数
+            Console.WriteLine("area: {0:f3}", area);
+            double area2 = Math.PI * r * r;
+            Console.WriteLine("area2: {0:f3}", area2);
+            double area3 = CLASS_PI * r * r;
+            Console.WriteLine("area3: {0:f3}", area3);
+
+            try
+            {
+                int i = Convert.ToInt32("abc");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("----Message----");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("----StackTrace----");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            try
+            {
+                int age = -10;
+                string ageType = GetAgeType(age);
+                Console.WriteLine(ageType);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("----Message----");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("----StackTrace----");
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+        }
+
+        static string GetAgeType(int age)
+        {
+            if (age >= 0 && age < 3)
+            {
+                return "婴幼儿";
+            }
+
+            else if (age >= 3 && age < 18)
+            {
+                return "青少年";
+            }
+            else if (age >= 18 && age <= 130)
+            {
+                return "成年人";
+            }
+            else if (age < 0)
+            {
+                throw new Exception("年龄不能小于0");
+            }
+
+            throw new Exception("年龄太大");
+        }
+    }
+}
