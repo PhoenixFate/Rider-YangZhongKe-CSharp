efe68bbca7f5524db4121bdc8b5d7ffac7ccca24
phoenix fate <sm516116978@outlook.com>
2020-02-19 12:35:23 +0800

    面向对象：类的相关特性

---
+++ Object-01-Class/Chat.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object_01_Class
+{
+    class Chat
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("-----chat-----");
+            Machine machine1 = new Machine();
+            machine1.Name = "robot";
+            machine1.Eat(5);
+            while (true)
+            {
+                String word = Console.ReadLine();
+                machine1.Speak(word);
+            }
+
+        }
+    }
+
+    class Machine
+    {
+        public string Name { set; get; }
+        private int FullLevel { set; get; }
+
+        public void SayHello()
+        {
+            Console.WriteLine("hello, my name is {0}", this.Name);
+        }
+
+        public void Eat(int level)
+        {
+            if (this.FullLevel > 10)
+            {
+                return;
+            }
+            FullLevel += level;
+        }
+
+        public void Speak(string str)
+        {
+            if (FullLevel <= 0)
+            {
+                Console.WriteLine("i need to eat something");
+                return;
+            }
+            if (str.Contains("姓名") || str.Contains("名字") || str.Contains("name"))
+            {
+                SayHello();
+            }
+            else if (str.Contains("女朋友"))
+            {
+                Console.WriteLine("机器人不谈恋爱");
+            }
+            else
+            {
+                Console.WriteLine("不知道你在说什么");
+            }
+            FullLevel--;
+        }
+
+    }
+}
---
+++ Object-01-Class/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Object_01_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // unsafe
+            // {
+            //     int number = 27;
+            //     int* pointerToNumber = &number;
+            //     int number2 = 27;
+            //     int* pointerToNumber2 = &number2;
+            //     Console.WriteLine($"Value of the variable: {number}");
+            //     Console.WriteLine($"Address of the variable: {(long)pointerToNumber:X}");
+            //     Console.WriteLine($"Address of the variable: {(long)pointerToNumber2:X}");
+            // }
+
+
+            Person p1 = new Person();
+            p1.Name = "abc";
+            Console.WriteLine(p1.Name);
+            p1.Age = 10;
+            Console.WriteLine(p1.Age);
+
+            p1.SayHello();
+
+            Console.WriteLine("----------------constructor----------------");
+            Person p2 = new Person("constructor");
+            Person p3 = new Person("constructor2",20);
+            Console.WriteLine(p2.Name);
+            Console.WriteLine("{0},{1}",p3.Name,p3.Age);
+        }
+    }
+
+
+    class Person
+    {
+
+        public int Height { get; set; }
+
+        private int age;
+        public int Age
+        {
+            set
+            {
+                //value就是传入的值
+                Console.WriteLine("set age");
+                age = value;
+            }
+            get
+            {
+                Console.WriteLine("get age");
+                return age;
+            }
+        }
+        public string Name { get; set; }
+
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+
+        public void SayHello()
+        {
+            Console.WriteLine("person say hello");
+        }
+    }
+}
