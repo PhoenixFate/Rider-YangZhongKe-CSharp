8872cc8de3fec65d833c271eef01d0fd5f87f114
phoenix fate <sm516116978@outlook.com>
2020-02-19 14:45:46 +0800

    命名空间

---
+++ Object-04-Namespace/Phoenix/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//namespace 不需要跟包同名
+namespace Object_04_Namespace.Phoenix
+{
+    class Person
+    {
+    }
+
+    class Dog
+    {
+
+    }
+}
---
+++ Object-04-Namespace/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using Object_04_Namespace.Phoenix;
+
+namespace Object_04_Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IList list = new ArrayList();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+            foreach (Object o in list)
+            {
+                Console.WriteLine(o);
+            }
+
+            Person p1 = new Person();
+            Console.WriteLine(p1);
+            Phoenix.Person p2 = new Phoenix.Person();
+            Console.WriteLine(p2);
+
+            //如果需要使用的类和当前类不在同一个namespace，则需要using Object_04_Namespace.Phoenix;
+            Dog dog = new Dog();
+            Console.WriteLine(dog);
+        }
+    }
+
+    class Person
+    {
+
+    }
+}
